f4ce732410ec3dc8445b8ecd467ad3f7901fcd04
Jumzer <bluevictor@hotmail.fr>
2021-12-03 12:49:08 +0100

    wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
+import { Box, Typography } from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-around",
+  },
+  text: {
+    color: theme.palette.primary.main,
+  },
+}));
 
 export default function BasicMenu() {
+  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -15,15 +29,18 @@ export default function BasicMenu() {
   };
 
   return (
-    <div>
+    <Box className={classes.container}>
       <Button
         id="basic-button"
         aria-controls="basic-menu"
         aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
+        aria-expanded={open ? "true" : undefined}
         onClick={handleClick}
       >
-        DOMAINE 4 PIERRES
+        <Typography className={classes.container}>
+          {" "}
+          DOMAINE 4 PIERRES{" "}
+        </Typography>
       </Button>
       <Menu
         id="basic-menu"
@@ -31,15 +48,25 @@ export default function BasicMenu() {
         open={open}
         onClose={handleClose}
         MenuListProps={{
-          'aria-labelledby': 'basic-button',
+          "aria-labelledby": "basic-button",
         }}
       >
-        <MenuItem onClick={handleClose}><Link to="/">Le domaine</Link></MenuItem>
-        <MenuItem onClick={handleClose}><Link to="/vins">Nos vins</Link></MenuItem>
-        <MenuItem onClick={handleClose}><Link to="/partenaires">Les partenaires</Link></MenuItem>
-        <MenuItem onClick={handleClose}><Link to="/galerie">Galerie</Link></MenuItem>
-        <MenuItem onClick={handleClose}><Link to="/contact">Contact</Link></MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/">Le domaine</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/vins">Nos vins</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/partenaires">Les partenaires</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/galerie">Galerie</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/contact">Contact</Link>
+        </MenuItem>
       </Menu>
-    </div>
+    </Box>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import home1 from '../../assets/home1.jpg';
-import './Home.css';
-import Carousell from '../../components/Carousel'
-
+import home1 from "../../assets/home1.jpg";
+import "./Home.css";
+import Carousell from "../../components/Carousel";
 
 const Home = () => {
-
   return (
     <div>
       <img src={home1} alt="" />
-      <h2>DOMAINE DES 4 PIERRES <br /> 11600 ARAGON <br /> EDOUARD FORTIN</h2>
-      <p>Pour déguster, commander ou pour toute demande d’information, <strong>
-  contactez nous </strong></p>
-      <Carousell/>
+      <h2>
+        DOMAINE DES 4 PIERRES <br /> 11600 ARAGON <br /> EDOUARD FORTIN
+      </h2>
+      <p>
+        Pour déguster, commander ou pour toute demande d’information,{" "}
+        <strong>contactez nous </strong>
+      </p>
+      <Carousell />
     </div>
-  )
-}
+  );
+};
 export default Home;
-
