1dcd28444559f28cf31b4158d78e8fdb70e06745
Jumzer <bluevictor@hotmail.fr>
2021-12-06 13:56:35 +0100

    first stone

--- src/pages/Wines.jsx
+++ src/pages/Wines.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import PageContainer from "components/PageContainer";
+import * as React from "react";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
-const Wines = () => <h1>Wines</h1>;
+const Wines = () => {
+  return <PageContainer></PageContainer>;
+};
 export default Wines;
